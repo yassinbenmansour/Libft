518aaa829d7852f67104b2a8936cbc43878d8ca0
yassine benmansour <61294442+yassinebenmnsour@users.noreply.github.com>
2022-07-08 08:57:26 +0100

    update

--- libft.h
+++ libft.h
@@ -19,4 +19,10 @@
 # include <unistd.h>
 # include <string.h>
 
-int 	ft_atoi(const char *str); 
+int 	ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int 	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int i);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
