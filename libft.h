674f9034c4567de61faeccc8278b0c4ee83f2f68
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-07-08 07:23:02 +0100

    libft ybenmans

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    libft.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybenmans <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 19:39:07 by ybenmans          #+#    #+#             */
+/*   Updated: 2022/06/08 19:39:15 by ybenmans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+int 	ft_atoi(const char *str); 
