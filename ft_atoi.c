f6b94ad4be8238a815247983d5492d5dabf778d9
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-07-09 08:17:02 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -22,5 +22,16 @@ int ft_atoi(const char *str)
 	sign = 1 ;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		resultat = resultat * 10 + (*str - '0');
+		str++;
+	}
+	return (resultat * sign);
 }
