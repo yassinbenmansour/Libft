8febee696775832e90db685ef8a313a871eefcdf
yassine benmansour <61294442+yassinebenmnsour@users.noreply.github.com>
2022-07-09 08:11:34 +0100

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,3 +13,14 @@
 
 #include "libft.h"
 
+int ft_atoi(const char *str)
+{
+	int nresult ;
+	int sign ;
+
+	result = 0 ;
+	sign = 1 ;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str
+}
