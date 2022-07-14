594560583b2c8ff03fe23ace79980ea2286a5a47
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-07-14 15:52:59 +0100

    push daghoghotii

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,3 +9,16 @@
 /*   Updated: 2021/11/26 19:37:13 by ybenmans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, (size * count));
+	return (ptr);
+}
